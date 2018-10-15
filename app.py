b5dfac72a0260d5221e47a8a99333647646df12a
Eniraich <43996445+Eniraich@users.noreply.github.com>
2018-10-15 22:42:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import time, datetime
 
 vk = vk_api.VkApi(login='+79001046313', password='dit2471',token='21ed4f26912217c25e30b271812a4f64dd076edb93dffd6ff82952a1fb1c4870b3ffe12294004914d5657')
 vk.auth()
-id_id = '361649916' #Леня
+dt = datetime.datetime.today()
+id_1 = '361649916' #Леня
+id_2 = '499686451'
+mess_2 = 'тест'
+a = dt.hour
 s = 'не забудь про фейт, каждый день в 16 по мск' #текст cообщения
 #meth = 'messages.send'
 values = {'out':1, 'count':100, 'time_offset':60}
@@ -19,6 +23,15 @@ def mes_datetime():
 
 done = True
 while done:
-    #s = mes_datetime()
-    f_messages(id_id,str(s))
-    time.sleep(1)
+    dt = datetime.datetime.today()
+    s = mes_datetime()
+    f_messages(id_1,str(s))
+    time.sleep(3500)
+    if dt.hour != b:
+        f_messages(id_2,mess_2)
+        f_messages(id_2,s)
+        b = dt.hour
+
+
+
+
